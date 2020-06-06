10adf760563182d1e3c7829c954cfe5e7ba24e75
suganthan <suganthan.friartuck.net>
2020-06-06 15:27:30 +0800

    Validate subsequence

--- src/array/TwoNumberSum.java
+++ src/array/TwoNumberSum.java
@@ -1,4 +1,4 @@
-package arary;
+package array;
 
 import java.util.Arrays;
 import java.util.HashMap;
---
+++ src/array/ValidateSubsequence.java
@@ -0,0 +1,33 @@
+package array;
+
+import java.util.List;
+
+/**
+ * Given two non-empty arrays of integers, write a function that determines
+ * whether the second array is sub-sequence of the first one.
+ *
+ * A subsequence of an array is a set of numbers that aren't necessarily adjacent in the array but that
+ * are in the same order as they appear in the array. For instance [1, 3, 4] form a subsequence of
+ * the array [1,2,3,4], and so do the numbers [2,4]. Note that a single number in an array itself
+ * are both valid sub-sequences of the array.
+ */
+public class ValidateSubsequence {
+    public static void main(String[] args) {
+        //A is the master data
+        //B is the data u need to validate
+        //Move the a
+        //Move the b when u found some element in a
+        //True if b-sequencing is equal to b sizing
+    }
+
+    public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
+        int masterDataSequence = 0;
+        int subDataSequence    = 0;
+        while (masterDataSequence < array.size() && subDataSequence < sequence.size()) {
+            if (array.get(masterDataSequence).equals(sequence.get(subDataSequence)))
+                subDataSequence++;
+            masterDataSequence++;
+        }
+        return subDataSequence == sequence.size();
+    }
+}

4d9a89df8f63776d7e3a134dd3bde7d72335d488
suganthan <suganthan.friartuck.net>
2020-06-06 22:25:07 +0800

    Is monotonic

---
+++ src/array/MonotonicArray.java
@@ -0,0 +1,25 @@
+package array;
+
+/**
+ * Write a function that takes in an array of integers and returns a boolean representing whether
+ * the array is monotonic.
+ *
+ * An array is said to be monotonic if its elements, from left to right are entirely
+ * non-increasing or entirely non-decreasing.
+ *
+ * [-1, -5, -10, -1100, -1100, -1101, -1102, -9001]
+ */
+public class MonotonicArray {
+    public static boolean isMonotonic(int[] array) {
+        boolean isIncreasing = true;
+        boolean isDecreasing = true;
+        for (int i = 1; i < array.length - 1; i++) {
+            if (array[i] < array[i - 1])
+                isIncreasing = false;
+
+            if (array[i] > array[i - 1])
+                isDecreasing = false;
+        }
+        return isIncreasing || isDecreasing;
+    }
+}

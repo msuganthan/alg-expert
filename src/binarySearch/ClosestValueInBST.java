70d35479bee29e899c1411146058ca2257c1378d
suganthan <suganthan.friartuck.net>
2020-06-07 09:45:05 +0800

    Closest Value in BST

---
+++ src/binarySearch/ClosestValueInBST.java
@@ -0,0 +1,10 @@
+package binarySearch;
+
+/**
+ * Write a function that takes in a binary search tree and a target integer value
+ * and return the closest value to that target value container in the BST.
+ *
+ * You can assume that there will be one closest value.
+ */
+public class ClosestValueInBST {
+}

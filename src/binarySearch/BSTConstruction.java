89f5e5ae1ddaa881724d58a2a5fe7a999a352f61
suganthan <suganthan.friartuck.net>
2020-06-07 23:50:28 +0800

    BST constructions

--- src/binarySearch/BSTConstruction.java
+++ src/binarySearch/BSTConstruction.java
@@ -57,7 +57,9 @@ public class BSTConstruction {
      *   return -1;
      * }
      *
-     * Remove:
+     * Remove: It is a two step process
+     * 1. Find the value
+     * 2. Remove the value
      * ========
      *
      * //it is important to maintain the parent node
@@ -87,14 +89,90 @@ public class BSTConstruction {
     static class BST {
         public int value;
         public BST left;
-        public BST rigt;
+        public BST right;
 
-        public BST(int value) {
-            this.value = value;
+        public BST(int vlue) {
+            this.value = vlue;
         }
 
-        public void remove(int value, BST parentNode) {
+        public BST insert(int value) {
+            BST currentNode = this;
+            while(currentNode != null) {
+                if (value < currentNode.value) {
+                    if (currentNode.left == null) {
+                        BST newNode      = new BST(value);
+                        currentNode.left = newNode;
+                        break;
+                    } else
+                        currentNode      = currentNode.left;
+                } else {
+                    if (currentNode.right == null) {
+                        BST newNode      = new BST(value);
+                        currentNode.right = newNode;
+                        break;
+                    } else
+                        currentNode      = currentNode.right;
+                }
+            }
+            return currentNode;
+        }
 
+        public boolean contains(int value) {
+            BST currentNode = this;
+            while (currentNode != null) {
+                if (value < currentNode.value)
+                    currentNode = currentNode.left;
+                else if (value > currentNode.value)
+                    currentNode = currentNode.right;
+                else //This one I am having doubts
+                    return true;
+            }
+            return false;
+        }
+
+        //When you call the remove method on the root node, the parent node is null.
+        public BST remove(int value, BST parentNode) {
+            BST currentNode = this;
+            while (currentNode != null) {
+                if (value < currentNode.value) {
+                    parentNode  = currentNode;
+                    currentNode = currentNode.left;
+                } else if (value > currentNode.value) {
+                    parentNode  = currentNode;
+                    currentNode = currentNode.right;
+                } else {
+                    //1. Has both child
+                    if (currentNode.left != null && currentNode.right != null) {
+                        currentNode.value = currentNode.right.getMinValue();
+                        currentNode.right.remove(currentNode.value, currentNode);
+                    } else if (parentNode == null) { //2. Removal of root
+                        if (currentNode.left != null) {
+                            currentNode.value  = currentNode.left.value;
+                            currentNode.right  = currentNode.left.right;
+                            currentNode.left  = currentNode.left.left;
+                        } else if (currentNode.right != null) {
+                            currentNode.value  = currentNode.right.value;
+                            currentNode.left  = currentNode.right.left;
+                            currentNode.right  = currentNode.right.right;
+                        } else {
+                            //Do nothing
+                        }
+
+                    } else if (parentNode.left == currentNode)
+                        parentNode.left = currentNode.left != null ? currentNode.left : currentNode.right;
+                      else if (parentNode.right == currentNode)
+                        parentNode.right = currentNode.left != null ? currentNode.left : currentNode.right;
+                    break;
+                }
+            }
+            return currentNode;
+        }
+
+        public int getMinValue() {
+            if (left == null)
+                return value;
+            else
+                return left.getMinValue();
         }
     }
 }

f25f67e4e2e4f68fe1db879a85ffcf148ab504f0
suganthan <suganthan.friartuck.net>
2020-06-12 08:17:41 +0800

    Node depth

---
+++ src/algo_expert/BinaryTree/NodeDepth.java
@@ -0,0 +1,31 @@
+package algo_expert.BinaryTree;
+
+import java.math.BigDecimal;
+
+/**
+ * This distance between a node in a binary tree and the tree's root is called the node's depth.
+ */
+public class NodeDepth {
+
+    static int nodeDepths(BinaryTree root) {
+        return nodeDepthHelper(root, 0);
+    }
+
+    static int nodeDepthHelper(BinaryTree root, int depth) {
+        if (root == null)
+            return 0;
+        return depth + nodeDepthHelper(root.left, depth + 1) + nodeDepthHelper(root.right, depth + 1);
+    }
+
+    static class BinaryTree {
+        int value;
+        BinaryTree left;
+        BinaryTree right;
+
+        BinaryTree(int value) {
+            this.value = value;
+            this.left  = null;
+            this.right = null;
+        }
+    }
+}

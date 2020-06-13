f833ecfc8f4673dee60df79ce58a44a544f2bbbf
suganthan <suganthan.friartuck.net>
2020-06-13 18:42:21 +0800

    River Size no implementation

---
+++ src/algo_expert/graphs/RiverSizes.java
@@ -0,0 +1,11 @@
+package algo_expert.graphs;
+
+/**
+ * You're given a two dimensional array of potentially unequal height and width containing only `0`
+ * and `1`s. Each `0` represents land and each 1 represents part of a river. A river consists of any
+ * number of 1's that are either horizontally or vertically adjacent(but not diagonally adjacent).
+ * The number of adjacent 1s forming a river determine its size.
+ */
+public class RiverSizes {
+
+}

7f653bf3614972d9ae22d32f41e2e051930d31c8
suganthan <suganthan.friartuck.net>
2020-06-14 11:46:46 +0800

    Insertion Sort

---
+++ src/algo_expert/sorting/InsertionSort.java
@@ -0,0 +1,23 @@
+package algo_expert.sorting;
+
+public class InsertionSort {
+
+    static int[] insertionSort(int[] array) {
+        if (array.length == 0)
+            return new int[] {};
+        for (int i = 0; i < array.length; i++) {
+            int j = i;
+            while (j > 0 && array[j] < array[j -1]) {
+                swap(array, j, j - 1);
+                j--;
+            }
+        }
+        return array;
+    }
+
+    static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}

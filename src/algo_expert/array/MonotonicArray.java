df3870c8ccd9864656f2c279354a1a7af5d27fa5
suganthan <suganthan.friartuck.net>
2020-07-10 18:09:54 +0800

    Monotonic array

--- src/algo_expert/array/MonotonicArray.java
+++ src/algo_expert/array/MonotonicArray.java
@@ -1,25 +1,24 @@
 package algo_expert.array;
 
 /**
- * Write a function that takes in an algo_expert.array of integers and returns a boolean representing whether
- * the algo_expert.array is monotonic.
+ * Write a function that takes in an array of integers and returns a boolean representing whether
+ * the array is monotonic.
  *
- * An algo_expert.array is said to be monotonic if its elements, from left to right are entirely
+ * An array is said to be monotonic if its elements, from left to right are entirely
  * non-increasing or entirely non-decreasing.
  *
  * [-1, -5, -10, -1100, -1100, -1101, -1102, -9001]
  */
 public class MonotonicArray {
     public static boolean isMonotonic(int[] array) {
-        boolean isIncreasing = true;
-        boolean isDecreasing = true;
-        for (int i = 1; i < array.length - 1; i++) {
+        boolean increasing = true;
+        boolean decreasing = true;
+        for (int i = 1; i < array.length; i++) {
             if (array[i] < array[i - 1])
-                isIncreasing = false;
-
+                decreasing = false;
             if (array[i] > array[i - 1])
-                isDecreasing = false;
+                increasing = false;
         }
-        return isIncreasing || isDecreasing;
+        return increasing || decreasing;
     }
 }

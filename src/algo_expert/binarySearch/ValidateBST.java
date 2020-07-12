70cb7758c814dd376210d9322cb46aa2971f61ea
suganthan <suganthan.friartuck.net>
2020-07-12 10:30:32 +0800

    Validate BST

--- src/algo_expert/binarySearch/ValidateBST.java
+++ src/algo_expert/binarySearch/ValidateBST.java
@@ -7,6 +7,41 @@ package algo_expert.binarySearch;
  */
 public class ValidateBST {
 
+    public static void main(String[] args) {
+        //Tree
+        //         10
+        //       /    \
+        //      5     15
+        //     / \   /  \
+        //    2   5 13  22
+        //   /        \
+        //  1         14
+
+        BST tree22  = new BST(22);
+        BST tree15  = new BST(15);
+        BST tree14  = new BST(14);
+        BST tree13  = new BST(13);
+        BST tree10  = new BST(10);
+        BST tree5   = new BST(5);
+        BST tree5_2 = new BST(5);
+        BST tree2   = new BST(2);
+        BST tree1   = new BST(1);
+
+        tree2.left = tree1;
+        tree5.left = tree2;
+        tree5.right = tree5_2;
+
+        tree10.left = tree5;
+        tree10.right = tree15;
+
+        tree15.left = tree13;
+        tree15.right = tree22;
+
+        tree13.right = tree14;
+
+        System.out.println(validateBst(tree10));
+    }
+
     //Tree
     //         10
     //       /    \
@@ -24,21 +59,29 @@ public class ValidateBST {
     public static boolean validateBst(BST tree) {
         //Left  tree is valid if all the value is lesser  than the current node.
         //Right tree is valid if all the value is greater than the current node.
-        return validateBst(tree, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        return isValid(tree, null, null);
     }
 
-    static boolean validateBst(BST tree, int minValue, int maxValue) {
-        if (tree == null)
+    public static boolean isValid(BST tree, Integer lowerLimit, Integer upperLimit) {
+		if (tree == null)
             return true;
 
-        //[Left tree] If the value is lesser  than           min value
-        //[rigt tree] If the value is greater than or equals max value
-        //Then the tree is invalid
-        if (tree.value < minValue || tree.value >= maxValue)
+        /**
+         * For left tree
+         * If left tree's value is greater than or equals upperLimit return false.
+         */
+        if (upperLimit != null && tree.value >= upperLimit)
             return false;
 
-        return validateBst(tree.left, minValue, tree.value) && validateBst(tree.right, tree.value, maxValue) ;
-    }
+        /**
+         * For right tree
+         * If right tree's value is lesser then lower limit return false
+         */
+        if (lowerLimit != null && tree.value < lowerLimit)
+            return false;
+
+        return isValid(tree.left, lowerLimit, tree.value) && isValid(tree.right, tree.value, upperLimit);
+	}
 
     static class BST {
         public int value;
